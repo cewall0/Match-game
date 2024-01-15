5f2617e318bef28050dbfeec64ec9062b24b641a
Chad Wallace <cewall0@gmail.com>
2024-01-15 09:58:54 -0500

    Initial Commit

---
+++ Match game/Match_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Match_gameApp.swift
+//  Match game
+//
+//  Created by Chad Wallace on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Match_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
